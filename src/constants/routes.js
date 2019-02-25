9705d689d7a8e5415a90c89991a2ef172ae1e79d
Ivan <ivankacir@gmail.com>
2019-02-25 12:37:10 -0500

    Add member interview page and routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,4 +3,4 @@ export const SKILL_SELECT = '/select-skills'
 export const SIGNUP = '/signup'
 export const GET_INVOLVED = '/get-involved'
 export const DASHBOARD = '/dashboard'
-
+export const MEMBER_INTERVIEW = '/member-interview'
---
+++ src/pages/member-interview.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MemberInterview = () => {
+    return (
+        <div>Member Interview</div>
+    )
+}
+
+export default MemberInterview;
