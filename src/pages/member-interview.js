58990b86c1b73ccfa80441fc921ccb388ce40551
Ivan <ivankacir@gmail.com>
2019-02-26 12:22:47 -0500

    Add member interview page content

--- src/pages/member-interview.js
+++ src/pages/member-interview.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { SmallLogo } from "../components/OnboardingStyles/LandingPageLogo";
+import ProgressBar from "../components/OnboardingStyles/ProgressBar";
+
+import colorlogo from "../assets/images/colorlogo.png";
+import meetMember from "../assets/images/006-interview.svg";
+import { CardHolder } from "../components/OnboardingStyles/CardHolder";
+import { OnboardingButton } from "../components/OnboardingStyles/OnboardingButtons";
 
 const MemberInterview = () => {
-    return (
-        <div>Member Interview</div>
-    )
-}
+  return (
+    <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+      <SmallLogo src={colorlogo} />
+      <ProgressBar currentStep={2} />
+      <div style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}>
+        <CardHolder>
+          <h1 style={{ textAlign: "center", margin: "auto", padding: "1vh" }}>
+            Meet with a Member
+          </h1>
+          <img
+            src={meetMember}
+            style={{ height: "10vh", margin: "auto", padding: "4vh" }}
+            alt="meet a member"
+          />
+        </CardHolder>
+        <p>
+          Thanks for taking the time to fill out your information! You're almost
+          there.
+        </p>
+        <CardHolder>
+          <div>
+            <h4>Ready to take the next step?</h4>
+            <Link to="#">
+              <OnboardingButton
+                style={{
+                  padding: "1vh",
+                  fontSize: "1em",
+                  backgroundColor: "#91c63f"
+                }}
+              >
+                Meet a member
+              </OnboardingButton>
+            </Link>
+          </div>
+          <div>
+            <h4>Not quite ready yet?</h4>
+            <Link to="/dashboard">
+              <OnboardingButton
+                style={{
+                  fontSize: "1em",
+                  padding: "1vh",
+                  backgroundColor: "#4a99cb"
+                }}
+              >
+                Continue to Dashboard
+              </OnboardingButton>
+            </Link>
+          </div>
+        </CardHolder>
+      </div>
+    </div>
+  );
+};
 
 export default MemberInterview;
