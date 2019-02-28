cbfc4585fca78d232695ea11679f26821330f286
Ivan <ivankacir@gmail.com>
2019-02-28 13:42:10 -0500

    Add book a time modal to the member interview page

--- src/pages/member-interview.js
+++ src/pages/member-interview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import { SmallLogo } from "../components/OnboardingStyles/LandingPageLogo";
@@ -9,59 +9,56 @@ import meetMember from "../assets/images/006-interview.svg";
 import { CardHolder } from "../components/OnboardingStyles/CardHolder";
 import { OnboardingButton } from "../components/OnboardingStyles/OnboardingButtons";
 
-const MemberInterview = () => {
-  return (
-    <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
-      <SmallLogo src={colorlogo} />
-      <ProgressBar currentStep={2} />
-      <div style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}>
-        <CardHolder>
-          <h1 style={{ textAlign: "center", margin: "auto", padding: "1vh" }}>
-            Meet with a Member
-          </h1>
-          <img
-            src={meetMember}
-            style={{ height: "10vh", margin: "auto", padding: "4vh" }}
-            alt="meet a member"
-          />
-        </CardHolder>
-        <p>
-          Thanks for taking the time to fill out your information! You're almost
-          there.
-        </p>
-        <CardHolder>
-          <div>
-            <h4>Ready to take the next step?</h4>
-            <Link to="#">
-              <OnboardingButton
-                style={{
-                  padding: "1vh",
-                  fontSize: "1em",
-                  backgroundColor: "#91c63f"
-                }}
-              >
-                Meet a member
-              </OnboardingButton>
-            </Link>
-          </div>
-          <div>
-            <h4>Not quite ready yet?</h4>
-            <Link to="/dashboard">
-              <OnboardingButton
-                style={{
-                  fontSize: "1em",
-                  padding: "1vh",
-                  backgroundColor: "#4a99cb"
-                }}
-              >
-                Continue to Dashboard
-              </OnboardingButton>
-            </Link>
-          </div>
-        </CardHolder>
+import InterviewModal from "../components/InterviewModal";
+import { ModalProvider } from "styled-react-modal";
+
+class MemberInterview extends Component {
+  render() {
+    return (
+      <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+        <SmallLogo src={colorlogo} />
+        <ProgressBar currentStep={2} />
+        <div style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}>
+          <CardHolder>
+            <h1 style={{ textAlign: "center", margin: "auto", padding: "1vh" }}>
+              Meet with a Member
+            </h1>
+            <img
+              src={meetMember}
+              style={{ height: "10vh", margin: "auto", padding: "4vh" }}
+              alt="meet a member"
+            />
+          </CardHolder>
+          <p>
+            Thanks for taking the time to fill out your information! You're
+            almost there.
+          </p>
+          <CardHolder>
+            <div>
+              <h4>Ready to take the next step?</h4>
+              <ModalProvider>
+                <InterviewModal />
+              </ModalProvider>
+            </div>
+            <div>
+              <h4>Not quite ready yet?</h4>
+              <Link to="/dashboard">
+                <OnboardingButton
+                  style={{
+                    fontSize: "1em",
+                    padding: "1vh",
+                    backgroundColor: "#4a99cb"
+                  }}
+                >
+                  Continue to Dashboard
+                </OnboardingButton>
+              </Link>
+            </div>
+          </CardHolder>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MemberInterview;
