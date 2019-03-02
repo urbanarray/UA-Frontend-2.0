75ba00466feef6b0a2bd9c7780a06aaaac18a29c
Hannah Werman <hanna.w28@gmail.com>
2019-03-02 14:48:13 +0700

    build

---
+++ build/precache-manifest.6c3e39ecfe983371651f0dc49e5dae9d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "66efc7e2cbbe8dfe8efadad9478d57f3",
+    "url": "/static/media/stockImage1.66efc7e2.jpg"
+  },
+  {
+    "revision": "1f757a05fd1177933e13",
+    "url": "/static/css/main.340a44fa.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f113a8f954029fef3187",
+    "url": "/static/js/2.f113a8f9.chunk.js"
+  },
+  {
+    "revision": "c8941999f88087554e3a909463c13d48",
+    "url": "/static/media/colorlogo.c8941999.png"
+  },
+  {
+    "revision": "8d7851fbdf12abf10620d4dfcf83796b",
+    "url": "/static/media/006-interview.8d7851fb.svg"
+  },
+  {
+    "revision": "72e2121f060194b3013ed63a4bfa1c56",
+    "url": "/static/media/logo.72e2121f.png"
+  },
+  {
+    "revision": "1f757a05fd1177933e13",
+    "url": "/static/js/main.1f757a05.chunk.js"
+  },
+  {
+    "revision": "a25aa9384eb8bcfc0726475c17043afe",
+    "url": "/static/media/stockImage2.a25aa938.jpg"
+  },
+  {
+    "revision": "487ec5751d40e05d65a3c23570c1e9d3",
+    "url": "/static/media/001-study.487ec575.svg"
+  },
+  {
+    "revision": "6c5e2cb16d22b6ab86c82062a8017416",
+    "url": "/static/media/002-network.6c5e2cb1.svg"
+  },
+  {
+    "revision": "451f642c1a448fa07f00fa9476a3b135",
+    "url": "/static/media/003-social-care.451f642c.svg"
+  },
+  {
+    "revision": "b12caf9ab86c69d7fd7eb58c76a4afa0",
+    "url": "/static/media/004-connection.b12caf9a.svg"
+  },
+  {
+    "revision": "a1487b62bdde553019e58b2daa9bd1bc",
+    "url": "/static/media/005-skills.a1487b62.svg"
+  },
+  {
+    "revision": "3dcf303cc0d5a9c1e966060a300e39d2",
+    "url": "/index.html"
+  }
+];
